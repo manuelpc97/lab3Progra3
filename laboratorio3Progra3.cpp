499d9b05b08125b0d34241c0a9a6d9a64fa0a708
manuelpc97 <manuelpc97@unitec.edu>
2016-02-05 13:26:06 -0600

    Probando el random

---
+++ laboratorio3Progra3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main(int argc, char*argv[]){
+bool jugar = true;
+bool seguir = true;
+srand(time(NULL));
+int contador=0;
+int numero = 0;
+int intento = 0;
+
+//while(jugar ==true){
+	
+//}
+numero  = -500 + rand()%(501+500);
+        cout<<numero<<endl;
+return 0;
+}
